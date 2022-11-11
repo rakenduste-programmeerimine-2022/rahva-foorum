46621b781de3619f00afff6417b5bc82b7569a23
Kristjan Tamm <90192493+TammKristjan@users.noreply.github.com>
2022-11-11 12:41:30 +0200

    Added firebase database connection

---
+++ backend/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeOFHhGWyqTnfU4P3YixOox6fuEmwL6Fo",
+  authDomain: "rahva-foorum.firebaseapp.com",
+  projectId: "rahva-foorum",
+  storageBucket: "rahva-foorum.appspot.com",
+  messagingSenderId: "199237889271",
+  appId: "1:199237889271:web:e595c5f8a33cf28f1577f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
