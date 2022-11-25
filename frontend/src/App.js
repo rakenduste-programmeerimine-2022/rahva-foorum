038d7d133a09bd7b5b28816bff68e0a1d3d98281
Kristjan Tamm <90192493+TammKristjan@users.noreply.github.com>
2022-11-25 19:45:16 +0200

    Create post, text editor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,9 @@
-import logo from './logo.svg';
+import Navbar from "./components/Navbar.js"
+import Mail from "./pages/Mail.js"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import NewPost from "./pages/NewPost"
+import Sidenav from './components/SideNav.js';
 import './App.css';
 import React, {useState, useEffect} from 'react';
 import { db } from "./firebase";
@@ -20,17 +25,29 @@ function App() {
   });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Getting data from db:
-          {posts[0] != null &&
-            posts[0].data.asukoht
-          }
-        </p>
-      </header>
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mail" element={<Mail />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/newpost" element={<NewPost />} />
+        </Routes>
+      </div>
+      <div className="App">
+      <Sidenav/>
+      <main>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/mail" element={<Mail />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/newpost" element={<NewPost />} />
+      </Routes>
+      </main>
+     
     </div>
+    </>
   );
 }
 
