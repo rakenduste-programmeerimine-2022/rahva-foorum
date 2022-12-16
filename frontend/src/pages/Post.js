e9a7800d27062aa8a05f761c41a7f35a40d5c1f9
Kristjan Tamm <90192493+TammKristjan@users.noreply.github.com>
2022-12-16 16:35:25 +0200

    comment field

--- frontend/src/pages/Post.js
+++ frontend/src/pages/Post.js
@@ -1,5 +1,5 @@
 
-import { Container, Stack, Typography } from '@mui/material';
+import { Container, Stack, Typography, TextField, Button } from '@mui/material';
 export default function Post() {
 
     return(
@@ -32,6 +32,17 @@ export default function Post() {
                         <Typography variant="body1" color="initial" class="content">
                             Cras laoreet vitae arcu at facilisis. Nulla efficitur odio nec velit rhoncus, ac imperdiet nisi convallis. Aliquam rutrum non orci vel blandit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus efficitur tempor urna, et consectetur urna convallis vel. Praesent rhoncus arcu a tincidunt convallis. Ut commodo magna eget fringilla sagittis. Quisque auctor ante mattis felis imperdiet dictum. Sed tristique egestas nibh finibus tincidunt. Nullam tortor diam, viverra sed elementum at, gravida suscipit nisl. Nam dictum ante ac augue semper, sit amet cursus metus tempor.
                         </Typography>
+                    </Container>
+
+                    <Container sx={{ border: 1 }}>
+                        <TextField
+                        placeholder="MultiLine with rows: 2 and rowsMax: 4"
+                        multiline
+                        rows={2}
+                        maxRows={4}
+                        fullWidth
+                        />
+                        <Button sx={{ backgroundColor: 'black', '&:hover': { backgroundColor:'green' }}} variant="contained">Lisa kommentaar</Button>
                     </Container>
                 </Stack>
             </Container>
